6ba9b43c1b4d5cb1003fc1f516a31098346f902f
CUAUV <leader@cuauv.org>
2018-06-13 23:56:57 +0000

    Merge pull request #22 in SOF/subcode from pooltest-castor-2018-06-13 to master

--- mission/missions/gate.py
+++ mission/missions/gate.py
@@ -1,27 +1,22 @@
-from mission.framework.combinators import Sequential
-from mission.framework.movement import Depth, VelocityX
-from mission.framework.position import MoveXRough
-from mission.framework.primitive import Log, Zero
-from mission.framework.timing import Timed
+from mission.framework.combinators import Sequential, Concurrent
+from mission.framework.movement import RelativeToInitialHeading, Depth, VelocityY
+from mission.framework.position import MoveX
+from mission.framework.primitive import Log
+from mission.framework.targeting import PIDLoop
 from mission.framework.task import Task
-from mission.constants.config import gate as constants
 
-class ArtemisGate(Task):
-    def on_first_run(self, *args, **kwargs):
-        self.use_task(Sequential(
-            Log('Going to gate depth'),
-            Depth(constants.depth),
-            Log('Moving through gate'),
-            Timed(VelocityX(0.4), constants.artemis_vel_time),
-            Zero(),
-        ))
+import shm
 
-class ApolloGate(Task):
+results_groups = shm.yellow_buoy_results
+
+
+class XTarget(Task):
     def on_first_run(self, *args, **kwargs):
-        self.use_task(Sequential(
-            Log('Going to gate depth'),
-            Depth(constants.depth),
-            Log('Moving through gate'),
-            Timed(VelocityX(0.4), constants.vel_time),
-            Zero(),
-        ))
+        self.pid_loop_x = PIDLoop(output_function=VelocityY(), negate=True)
+
+    def on_run(self, *args, **kwargs):
+        point = results_groups.center_x.get()
+        self.pid_loop_x(input_value=point, target=0, p=0.8, deadband=0.01875)
+
+
+target = Concurrent(Depth(1), XTarget())

e0023a5ece7275b98007fba02d60fb7a5b04bfd6
CUAUV <leader@cuauv.org>
2019-03-04 00:22:31 +0000

    Merge pull request #71 in SOF/subcode from start-modules-fix to master

--- misc/startm.sh
+++ misc/startm.sh
@@ -3,9 +3,17 @@
 
 LOGS=$CUAUV_LOG/current
 MDIR=vision/modules
+CMD="${CUAUV_SOFTWARE}/${MDIR}/$1.py $2"
+
+shutdown() {
+    kill $child_pid
+    exit 0
+}
+
+trap shutdown INT TERM HUP
 
 while :; do
-    CMD="${CUAUV_SOFTWARE}/${MDIR}/$1.py $2"
-    stdbuf -oL -eL $CMD >> $LOGS/$3.log
-    sleep 1;
+    stdbuf -oL -eL $CMD >> $LOGS/$3.log &
+    child_pid=$!
+    wait
 done

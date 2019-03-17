aea06c5afc1284cd8780b9577dcace06404db105
CUAUV <leader@cuauv.org>
2019-03-17 23:32:29 +0000

    Merge pull request #73 in SOF/subcode from auvlog to master

--- auvlog/daemon.py
+++ auvlog/daemon.py
@@ -12,7 +12,7 @@ import os
 
 from auvlog import config
 
-redis = redis.StrictRedis()
+redis_client = redis.StrictRedis()
 
 socketS = nanomsg.Socket(nanomsg.SUB)
 socketS.set_string_option(nanomsg.SUB, nanomsg.SUB_SUBSCRIBE, '')
@@ -21,16 +21,21 @@ socketS.bind("tcp://*:{0}".format(config.CLIENT_PORT))
 socketP = nanomsg.Socket(nanomsg.PUB)
 socketP.bind("tcp://*:{0}".format(config.SERVER_PORT))
 
+
 def push_to_file(entry):
-  try:
-    f = open(os.path.join(os.environ['CUAUV_LOG'], 'current/auvlog.log'), 'ab')
-    f.write(entry + b'\n')
-  except IOError:
-    print('Warning: Unable to write log entry to file.')
+    try:
+        f = open(os.path.join(os.environ['CUAUV_LOG'], 'current/auvlog.log'), 'ab')
+        f.write(entry + b'\n')
+    except IOError:
+        print('Warning: Unable to write log entry to file.', flush=True)
+
 
 while True:
     res = socketS.recv()
-    redis.lpush(config.KEY, res)
-    redis.ltrim(config.KEY, 0, int(1e5))
+    try:
+        redis_client.lpush(config.KEY, res)
+        redis_client.ltrim(config.KEY, 0, int(1e5))
+    except redis.exceptions.ConnectionError:
+        print('Warning: Unable to connect to redis instance', flush=True)
     push_to_file(res)
     socketP.send(res)

85d7d64127a0d0466d7d424db425c2477c500ba9
CUAUV <leader@cuauv.org>
2019-06-07 19:27:51 +0000

    Merge pull request #77 in SOF/subcode from docker-user-override-default to master

--- docker/config.py
+++ docker/config.py
@@ -1,5 +1,21 @@
 from pathlib import Path, PurePosixPath
 
+"""
+You can override these defaults: create a new file called `user.py` (in this
+folder, `docker`) with a dict called `config`. Any keys defined in this dict
+will override the default ones below. An example user.py:
+
+```
+from pathlib import Path, PurePosixPath
+
+config = {
+    "WORKSPACE_DIRECTORY": Path("~/path/to/my/repo/").expanduser(),
+}
+```
+
+We use a separate file so that we don't track user-specific changes.
+"""
+
 defaults = {
     "WORKSPACE_DIRECTORY": Path("~/cuauv/workspaces").expanduser(),
     "CONTAINER_WORKSPACE_DIRECTORY": PurePosixPath("/home/software/cuauv/workspaces"),
@@ -10,6 +26,14 @@ defaults = {
     "BRANCH": "master",
 }
 
+try:
+   import user
+   if hasattr(user, 'config'):
+      conf = dict(defaults, **user.config)
+   else:
+      conf = defaults
+except ImportError:
+   conf = defaults
 
 def get_config(key):
-   return defaults[key]
+   return conf[key]
